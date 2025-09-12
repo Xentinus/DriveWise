dd9b854f6efb9800af0e56cc211ca8286bf5e9df
Béla Kellner <bela.kellner@icloud.com>
2025-09-12 18:00:13 +0200

    init

--- obj/Debug/net9.0/DriveWise.AssemblyInfo.cs
+++ obj/Debug/net9.0/DriveWise.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DriveWise")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dafaced5d299a26fe246035167675cacf5a27549")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ca84404462a3569bca07f48fe9f894cf27b6900")]
 [assembly: System.Reflection.AssemblyProductAttribute("DriveWise")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DriveWise")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
