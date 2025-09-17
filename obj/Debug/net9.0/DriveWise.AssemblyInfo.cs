ec177788d684d707830e0e9477363fd1ebb73908
Béla Kellner <bela.kellner@icloud.com>
2025-09-17 21:34:52 +0200

    Refactor CSS and update static web asset configurations
    - Updated the fingerprint and integrity values for the site.css asset in the static web assets props.
    - Renamed the site.css asset to site.y4cl5nz2gi.css for better cache management.
    - Enhanced responsive design in site.css:
      - Added media queries for tablet and mobile adjustments.
      - Adjusted widths and paddings for search and weather controls to improve layout on different screen sizes.

--- obj/Debug/net9.0/DriveWise.AssemblyInfo.cs
+++ obj/Debug/net9.0/DriveWise.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DriveWise")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+032e41a88e510e457f4e9adf5d520e621fb71a79")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a683bca1c3a5f8e9c2beb385a86f9aa8e6e4cac")]
 [assembly: System.Reflection.AssemblyProductAttribute("DriveWise")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DriveWise")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
